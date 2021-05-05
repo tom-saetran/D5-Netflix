8af41de25d9cc12111b379bf275f0d40ebeeab05
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-05-05 13:19:05 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import Footer from './Components/Footer';
-import NavBar from './Components/NavBar';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MovieContainer from './Components/MovieContainer';
-import Galleries from './Components/Galleries';
+import Footer from "./Components/Footer"
+import NavBar from "./Components/NavBar"
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import MovieContainer from "./Components/MovieContainer"
+import Galleries from "./Components/Galleries"
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <MovieContainer />
-      <Galleries movie="Harry Potter" />
-      <Galleries movie="Lord of the Rings" />
-      <Galleries movie="Batman" />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar />
+            <MovieContainer />
+            <Galleries movie="Harry Potter" />
+            <Galleries movie="Lord of the Rings" />
+            <Galleries movie="Batman" />
+            <Footer />
+        </div>
+    )
 }
 
-export default App;
+export default App
